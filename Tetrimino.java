a206d59caae4447c339187a2a3b92fd9e61cde60
John Dogit config --global user.name John <jlee01@stuy.edu>
2019-01-04 14:35:41 -0500

    Created skeleton of Tetrimino.java

---
+++ Tetrimino.java
@@ -0,0 +1,22 @@
+public class Tetrimino{
+
+  private Piece[] pieces;
+  private int centerX;
+  private int centerY;
+
+  public Tetrimino(Piece[]){
+
+  }
+
+  public void rotate(){
+    
+  }
+
+  public int getX(){
+
+  }
+
+  public int getY(){}
+
+  public int moveDown(){}
+}
