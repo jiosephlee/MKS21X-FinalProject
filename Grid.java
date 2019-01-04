0bab01c3ae13277066de6884051a31f7f29937bb
qtgeo1248 <gzhou00@stuy.edu>
2019-01-04 14:22:52 -0500

    started making the hold and next part of toString

--- Grid.java
+++ Grid.java
@@ -8,11 +8,19 @@ public class Grid {
 
     public String toString() {
         String toReturn = "---------------------\n";
+        String hold = "";
+        String next = "";
+        for (int i = 0; i < 9; i++) {
+            if (i == 0 || i == 8) {
+                hold += "---------";
+            }
+        }
         for (int i = 4; i < grid.length; i++) { //remember first 4 rows are hidden
             for (int j = 0; j < grid[i].length; j++) {
                 toReturn += "|" + grid[i][j].toString();
             }
-            toReturn += "|\n";
+            toReturn += "|";
+            toReturn += "\n";
         }
         return toReturn + "---------------------";
     }
