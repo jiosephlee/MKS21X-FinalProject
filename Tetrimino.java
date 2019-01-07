93204bbd3344354e0ac86c173a75f8e9c7676b60
Joseph Lee <jlee01@stuy.edu>
2019-01-07 02:13:29 -0500

    almost finished Tetrimino.java

--- Tetrimino.java
+++ Tetrimino.java
@@ -6,17 +6,28 @@ public class Tetrimino{
 
   public Tetrimino(Piece[] input){
     pieces = input;
+    int sumx = 0;
+    int sumy = 0;
+    for (int x = 0; x < pieces.size; x++){
+      sumx += pieces[x].getX();
+      sumy += pieces[x].getY();
+    }
+    centerX = (double) sumx / pieces.size;
+    centerY = sumy / pieces.size;
   }
 
   public void rotate(){
-
+    
   }
 
   public int getX(){
-
+    return centerX;
   }
 
-  public int getY(){}
+  public int getY(){
+    return centerY;
+  }
+  public int moveDown(){
 
-  public int moveDown(){}
+  }
 }
