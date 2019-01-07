c6089332f3c16c539d8dceae131cf9633344a6d3
qtgeo1248 <gzhou00@stuy.edu>
2019-01-07 14:42:26 -0500

    fixed all the tabbing

--- Tetrimino.java
+++ Tetrimino.java
@@ -1,36 +1,38 @@
 public class Tetrimino{
 
-  private Piece[] pieces;
-  private double centerX;
-  private double centerY;
+    private Piece[] pieces;
+    private double centerX;
+    private double centerY;
 
-  public Tetrimino(Piece[] input){
-    pieces = input;
-    int sumx = 0;
-    int sumy = 0;
-    for (int x = 0; x < pieces.length; x++){
-      sumx += pieces[x].getX();
-      sumy += pieces[x].getY();
+    public Tetrimino(Piece[] input){
+        pieces = input;
+        int sumx = 0;
+        int sumy = 0;
+        for (int x = 0; x < pieces.length; x++){
+            sumx += pieces[x].getX();
+            sumy += pieces[x].getY();
+        }
+        centerX = (double)sumx / pieces.length;
+        centerY = (double)sumy / pieces.length;
+  }
+
+    public void rotate(){
+        for (int i = 0; i < pieces.length; i++) {
+            
+        }
     }
-    centerX = (double)sumx / pieces.length;
-    centerY = (double)sumy / pieces.length;
-  }
 
-  public void rotate(){
-    
-  }
-
-  public double getX(){
-    return centerX;
-  }
-
-  public double getY(){
-    return centerY;
-  }
-  public void moveDown(){
-    for (int x = 0; x < pieces.length; x++){
-      pieces[x].moveDown();
+    public double getX(){
+        return centerX;
     }
-    centerY--;
-  }
+
+    public double getY(){
+        return centerY;
+    }
+    public void moveDown(){
+        for (int x = 0; x < pieces.length; x++){
+            pieces[x].moveDown();
+        }
+        centerY--;
+    }
 }
