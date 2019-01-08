73646cbd24a74651e6c70f7dbca10c9cef0b40b1
qtgeo1248 <gzhou00@stuy.edu>
2019-01-07 22:06:09 -0500

    finsihed moveright and moveleft for tet

--- Tetrimino.java
+++ Tetrimino.java
@@ -8,9 +8,9 @@ public class Tetrimino{
         pieces = input;
         int sumx = 0;
         int sumy = 0;
-        for (int x = 0; x < pieces.length; x++){
-            sumx += pieces[x].getX();
-            sumy += pieces[x].getY();
+        for (int i = 0; i < pieces.length; i++){
+            sumx += pieces[i].getX();
+            sumy += pieces[i].getY();
         }
         centerX = (double)sumx / pieces.length;
         centerY = (double)sumy / pieces.length;
@@ -32,23 +32,35 @@ public class Tetrimino{
     }
 
     public void moveDown(){
-        for (int x = 0; x < pieces.length; x++){
-            pieces[x].moveDown();
+        for (int i = 0; i < pieces.length; i++){
+            pieces[i].moveDown();
         }
         centerY--;
     }
 
     public void moveRight() {
-        for (int x = 0; x < pieces.length; x++) {
-            pieces[x].moveRight();
+        for (int i = 0; i < pieces.length; i++) {
+            pieces[i].moveRight();
         }
         centerX++;
     }
+    public void moveRight(int x) {
+        for (int i = 0; i < pieces.length; i++) {
+            pieces[x].moveRight(x);
+        }
+        centerX += x;
+    }
 
     public void moveLeft() {
-        for (int x = 0; x < pieces.length; x++) {
-            pieces[x].moveLeft();
+        for (int i = 0; i < pieces.length; i++) {
+            pieces[i].moveLeft();
         }
         centerX--;
+    }
+    public void moveLeft(int x) {
+        for (int i = 0; i < pieces.length; i++) {
+            pieces[i].moveLeft(x);
+        }
+        centerX -= x;
     }
 }
