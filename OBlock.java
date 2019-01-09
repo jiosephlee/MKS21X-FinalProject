9909d7dd38bda635cd855b8140d96fa9fc1493e4
qtgeo1248 <gzhou00@stuy.edu>
2019-01-08 22:55:14 -0500

    made the TBlcok class

--- OBlock.java
+++ OBlock.java
@@ -1,4 +1,11 @@
 public class OBlock extends Tetrimino {
+    /* this is the shape:
+    -----
+    | | |
+    -----
+    | | |
+    -----
+    */
     public OBlock(int x, int y) { //x's and y's don't mean center, but starting point
         super(x + 0.5, y + 0.5);
         Piece a = new Piece(x, y);
---
+++ TBlock.java
@@ -0,0 +1,5 @@
+public class TBlock extends Tetrimino {
+    public TBlock(int x, int y) { //x's and y's don't mean center, but starting point
+        
+    }
+}
