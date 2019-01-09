1ee41a4c1030c64185ef4bc0386dc3f71b144ecf
qtgeo1248 <gzhou00@stuy.edu>
2019-01-08 22:47:26 -0500

    finished constructor and finished class

--- IBlock.java
+++ IBlock.java
@@ -1,3 +1,12 @@
 public class IBlock extends Tetrimino {
-    
+
+    public IBlock(int x, int y) { //x's and y's don't mean center, but starting point
+        super((double)(x + 2), (double)y - 0.5);
+        Piece a = new Piece(x, y); //precondition, x + 3 < 10 and y < 24
+        Piece b = new Piece(x + 1, y);
+        Piece c = new Piece(x + 2, y);
+        Piece d = new Piece(x + 3, y);
+        Piece[] toAdd = {a, b, c, d};
+        super.setPieces(toAdd);
+    }
 }
