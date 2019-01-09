b2bea5e1dc6bd8782862b63d11ca220bfb338516
qtgeo1248 <gzhou00@stuy.edu>
2019-01-08 23:35:48 -0500

    finished SBlock

--- SBlock.java
+++ SBlock.java
@@ -1,10 +1,10 @@
 public class SBlock extends Tetrimino{
     public SBlock(int x, int y) {
-        super();
-        Piece a = new Piece();
-        Piece b = new Piece();
-        Piece c = new Piece();
-        Piece d = new Piece();
+        super(x + 1, y + 1);
+        Piece a = new Piece(x, y);
+        Piece b = new Piece(x + 1, y);
+        Piece c = new Piece(x + 1, y + 1);
+        Piece d = new Piece(x + 2, y + 1);
         Piece[] toAdd = {a, b, c, d};
         super.setPieces(toAdd);
     }
