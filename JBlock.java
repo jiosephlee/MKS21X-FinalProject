954b5eb420728a85baafc80797e177aeecc3c80d
qtgeo1248 <gzhou00@stuy.edu>
2019-01-08 23:41:47 -0500

    finished LBlock

--- JBlock.java
+++ JBlock.java
@@ -1,7 +1,7 @@
 public class JBlock extends Tetrimino {
     public JBlock(int x, int y) {
         super(x + 1, y - 1);
-        Piece a = new Piece(x, y);
+        Piece a = new Piece(x, y); //preconditions: x < 8, y < 24
         Piece b = new Piece(x, y - 1);
         Piece c = new Piece(x + 1, y - 1);
         Piece d = new Piece(x + 2, y - 1);
--- LBlock.java
+++ LBlock.java
@@ -1,10 +1,10 @@
 public class LBlock extends Tetrimino {
     public LBlock(int x, int y) {
-        super();
-        Piece a = new Piece();
-        Piece b = new Piece();
-        Piece c = new Piece();
-        Piece d = new Piece();
+        super(x + 1, y);
+        Piece a = new Piece(x, y); //preconditions: x < 7, y < 24
+        Piece b = new Piece(x + 1, y);
+        Piece c = new Piece(x + 2, y);
+        Piece d = new Piece(x + 3, y);
         Piece[] toAdd = {a, b, c, d};
         super.setPieces(toAdd);
     }
@@ -12,7 +12,7 @@ public class LBlock extends Tetrimino {
         ---
         | |
     -------
-    | | | |
+    | |r| |
     -------
     */
 }
