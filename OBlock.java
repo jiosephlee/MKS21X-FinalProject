691026abb688edc97abc58b9a6c74e6121523226
qtgeo1248 <gzhou00@stuy.edu>
2019-01-08 23:07:07 -0500

    finished Tblock const. and class

--- OBlock.java
+++ OBlock.java
@@ -1,7 +1,7 @@
 public class OBlock extends Tetrimino {
     public OBlock(int x, int y) { //x's and y's don't mean center, but starting point
         super(x + 0.5, y + 0.5);
-        Piece a = new Piece(x, y);
+        Piece a = new Piece(x, y); //precondition: x < 9, y < 23
         Piece b = new Piece(x + 1, y);
         Piece c = new Piece(x, y + 1);
         Piece d = new Piece(x + 1, y + 1);
--- TBlock.java
+++ TBlock.java
@@ -1,11 +1,12 @@
 public class TBlock extends Tetrimino {
     public TBlock(int x, int y) { //x's and y's don't mean center, but starting point
-        super();
-        Piece a =
-        Piece b =
-        Piece c =
-        Piece d =
+        super(x + 1, y);
+        Piece a = new Piece(x, y); //precondition: x < 8, y < 23
+        Piece b = new Piece(x + 1, y);
+        Piece c = new Piece(x + 2, y);
+        Piece d = new Piece(x + 1, y + 1);
         Piece[] toAdd = {a, b, c, d};
+        super.setPieces(toAdd);
     }
     /* this is the shape:
       ---
