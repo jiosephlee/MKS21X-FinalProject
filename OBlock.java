71f7eb7ea076d807aef0ea0821bbffd308660b52
qtgeo1248 <gzhou00@stuy.edu>
2019-01-08 22:52:37 -0500

    finished Oblock const. and class

--- OBlock.java
+++ OBlock.java
@@ -1,10 +1,10 @@
 public class OBlock extends Tetrimino {
     public OBlock(int x, int y) { //x's and y's don't mean center, but starting point
-        super();
-        Piece a =
-        Piece b =
-        Piece c =
-        Piece d =
+        super(x + 0.5, y + 0.5);
+        Piece a = new Piece(x, y);
+        Piece b = new Piece(x + 1, y);
+        Piece c = new Piece(x, y + 1);
+        Piece d = new Piece(x + 1, y + 1);
         Piece[] toAdd = {a, b, c, d};
         super.setPieces(toAdd);
     }
