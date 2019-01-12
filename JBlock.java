a98c2c0aaea61577e31b41d656456c5256b1a8d0
qtgeo1248 <gzhou00@stuy.edu>
2019-01-12 09:10:15 -0500

    changed constructors

--- JBlock.java
+++ JBlock.java
@@ -1,10 +1,10 @@
 public class JBlock extends Tetrimino {
-    public JBlock(int x, int y) {
+    public JBlock(int x, int y, String colour) {
         super(x + 1, y - 1);
-        Piece a = new Piece(x, y); //preconditions: x < 8, y < 24
-        Piece b = new Piece(x, y - 1);
-        Piece c = new Piece(x + 1, y - 1);
-        Piece d = new Piece(x + 2, y - 1);
+        Piece a = new Piece(x, y, colour); //preconditions: x < 8, y < 24
+        Piece b = new Piece(x, y - 1, colour);
+        Piece c = new Piece(x + 1, y - 1, colour);
+        Piece d = new Piece(x + 2, y - 1, colour);
         Piece[] toAdd = {a, b, c, d};
         super.setPieces(toAdd);
     }
