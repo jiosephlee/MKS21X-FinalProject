91ebf5822267dde4cb94751429334c3000d2023d
Joseph Lee <jlee01@stuy.edu>
2019-01-20 22:49:31 -0500

    finished merging

--- Tetris.java
+++ Tetris.java
@@ -25,17 +25,6 @@ public class Tetris {
 		if (!isEnter) {
 			Screen screen = new DefaultTerminalFactory().createScreen();
 			screen.startScreen();
-<<<<<<< HEAD
-			long tStart = System.currentTimeMillis();
-			long lastSecond = 0;
-			long tEnd = System.currentTimeMillis();
-			long millis = tEnd - tStart;
-			long time2 = System.currentTimeMillis();
-			long time1 = System.currentTimeMillis();
-			long diff = time2 - time1;
-			long lasts = 0;
-			while (true) {
-=======
 			TextGraphics text = screen.newTextGraphics();
 			text.putString(1, 1, "____    __    ____  _______  __        ______   ______   .___  ___.  _______    .___________.  ______   ");
 			text.putString(1, 2, "\\   \\  /  \\  /   / |   ____||  |      /      | /  __  \\  |   \\/   | |   ____|   |           | /  __  \\  ");
@@ -75,7 +64,6 @@ public class Tetris {
 		long diff = time2 - time1;
 		long lasts = 0;
 		while (true) {
->>>>>>> 40d7579cf6713cc3a5d878935f6e147906a8ce97
 			time2 = System.currentTimeMillis();
 			diff = time2 - time1;
 
@@ -97,11 +85,7 @@ public class Tetris {
 				String storage = game.toString();
 				int xcor = 3;
 				int ycor = 5;
-<<<<<<< HEAD
 				for (int i = 0; i < storage.length(); i++){ //set up the game graphics
-=======
-				for (int i = 0; i < storage.length(); i++) {
->>>>>>> 40d7579cf6713cc3a5d878935f6e147906a8ce97
 					xcor++;
 					if (storage.charAt(i) == '\t') {
 						xcor += 3;
@@ -170,7 +154,6 @@ public class Tetris {
 				}
 			KeyStroke key = screen.pollInput();
 
-<<<<<<< HEAD
 			if (key != null) { //check for user input
 
 				if      (key.getKeyType() == KeyType.Escape) break;
@@ -219,50 +202,5 @@ public class Tetris {
 		screen.refresh();
 		}
 	screen.stopScreen();
-=======
-				if (key != null) {
-					if (key.getKeyType() == KeyType.Escape) {
-						break;
-					} else if (key.getKeyType() == KeyType.Character) {
-						if (key.getCharacter() == ' ') {
-							game.moveDown(1);
-						} else if (key.getCharacter() == 'c' && !game.getHeld()) {
-							game.setHold();
-							game.setDrop();
-							game.setNext();
-						} else {
-							game.setHeld(false);
-						}
-					} else if (key.getKeyType() == KeyType.ArrowLeft) {
-						game.moveLeft(1);//game.moveLeft();
-					} else if (key.getKeyType() == KeyType.ArrowRight) {
-						game.moveRight(1);//game.moveRight();
-					} else if (key.getKeyType() == KeyType.ArrowUp) {
-						game.rotateCW();
-					} else if (key.getKeyType() == KeyType.ArrowDown) {
-						game.rotateCCW();
-					}
-				}
-				if (game.isDoneDropping()) {
-					game.setInStone();
-					game.setDrop();
-					game.setNext();
-				}
-				tEnd = System.currentTimeMillis();
-				millis = tEnd - tStart;
-				if (millis / 1000 > lastSecond) {
-					lastSecond = millis / 1000;
-					game.moveDown(1);
-				}
-				int[] cleared = game.checkTetris();
-				if (cleared.length > 0) {
-					game.removeTetris(cleared);
-				}
-				screen.doResizeIfNecessary();
-				screen.refresh();
-			}
-		}
-		screen.stopScreen();
->>>>>>> 40d7579cf6713cc3a5d878935f6e147906a8ce97
 	}
 }
